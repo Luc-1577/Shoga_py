83550c7e7420a8e129687681cbd54743fd90dac7
Luc-1577 <142753165+Luc-1577@users.noreply.github.com>
2023-09-11 17:15:29 -0300

    Shoga Project.py

--- Shoga Project.py
+++ Shoga Project.py
@@ -1,5 +1,4 @@
-import tkinter as tk
-from tkinter import scrolledtext
+import customtkinter as CTk
 import datetime
 import webbrowser
 import requests
@@ -13,13 +12,6 @@ def get_current_date():
     now = datetime.datetime.now()
     current_date = now.strftime("%A(%d), %B(%m) %Y")
     return f"Shoga - It's {current_date}"
-
-def multi_command(command_str):
-    part = command_str.split("&&")
-    junt = ""
-    for i in part:
-        junt = junt + i
-    return junt
 
 def get_site_name(command_str):
     part = command_str.split(" ")
@@ -37,6 +29,7 @@ def open_site():
         response = requests.head(url)
         if response.status_code == 200:
             webbrowser.open(url)
+            return 'Ok ^^'
     except:
         pass
 
@@ -71,38 +64,45 @@ def get_bot_response(user_input):
         return "I don't know if I can do that, please type \"--help\" to see the commands"
 
 
+
+def initial(event):
+    chat_display.insert(CTk.END, f"Shoga - Hej, how can I help you?\n")
+    chat_display.configure(state=CTk.DISABLED)
+
+
 def send_message(event=None):
     user_input = user_input_entry.get()
-    chat_display.configure(state=tk.NORMAL)  
-    chat_display.insert(tk.END, f"You - {user_input}\n")
+    chat_display.configure(state=CTk.NORMAL)  
+    chat_display.insert(CTk.END, f"You - {user_input}\n")
     
     response = get_bot_response(user_input)
-    chat_display.insert(tk.END, f"Shoga - {response}\n")
-    chat_display.configure(state=tk.DISABLED)  
-    user_input_entry.delete(0, tk.END)  
+    chat_display.insert(CTk.END, f"Shoga - {response}\n")
+    chat_display.configure(state=CTk.DISABLED)  
+    user_input_entry.delete(0, CTk.END)  
 
     if "exit" in user_input.lower():
-        chat_display.configure(state=tk.DISABLED)
-        user_input_entry.config(state=tk.DISABLED)
-        send_button.config(state=tk.DISABLED)
-        root.after(1500, root.destroy)
+        chat_display.configure(state=CTk.DISABLED)
+        user_input_entry.configure(state=CTk.DISABLED)
+        send_button.configure(state=CTk.DISABLED)
+        root.after(1200, root.destroy)
 
-root = tk.Tk()
+root = CTk.CTk()
 root.title("Shoga - Chatbot")
-'''root.configure(background = '')  Alterar o fundo da parte de enviar mensagem'''
+CTk.set_appearance_mode("dark")
 root.geometry('600x435')
 root.resizable(True, True)
 root.minsize(width = 400, height = 435)
 
-chat_display = scrolledtext.ScrolledText(root, wrap=tk.WORD, state=tk.DISABLED) 
-chat_display.pack(fill=tk.BOTH, expand=True)
-'''chat_display.configure(background = '') Alterar o fundo do chat'''
+chat_display =CTk.CTkTextbox(root, wrap=CTk.WORD, state=CTk.NORMAL) 
+chat_display.pack(fill=CTk.BOTH, expand=True)
+chat_display.bind('<Motion>', initial)           
 
-user_input_entry = tk.Entry(root)
-user_input_entry.pack(fill=tk.X, pady=1)
+user_input_entry = CTk.CTkEntry(root)
+user_input_entry.pack(fill=CTk.X, pady=1)
+user_input_entry.bind('<Motion>', initial)  
 user_input_entry.bind("<Return>", send_message)  
 
-send_button = tk.Button(root, text="Send", command=send_message)
+send_button = CTk.CTkButton(root, text="Send", command=send_message)
 send_button.pack()
 
 root.mainloop()
